ae026f99e0bb5650e043474e8ef74d7b6f9bc74c
Thang Khai <tnkhai94@gmail.com>
2020-08-10 08:38:44 -0600

    save higher order

--- higher-order.js
+++ higher-order.js
@@ -21,7 +21,9 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
 */
 
 //Code Here
-let evenNumbers // = mixedNumbers.filter(/* Provide Your Callback Here */)
+let evenNumbers = mixedNumbers.filter(function(element, index, wholeArray){
+  return element % 2 === 0;
+});
 
 
 
@@ -44,7 +46,10 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 */
 
 //Code Here
-let postTaxPrices // = prices.map(/* Provide Your Callback Here );
+//let postTaxPrices = prices.map(function(element, index, wholeArray){
+//  return element * 1 + (taxRate / 100);
+//})
+//return postTaxPrices
 
 
 
@@ -63,7 +68,9 @@ const populations = [8175133, 3792621, 2695598, 2100263];
 */
 
 //Code Here
-let totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
+let totalPopulation = populations.reduce(function(runningTotal, curElement, curIndex, wholeArray){
+  return runningTotal + curElement
+})
 
 
 
@@ -89,7 +96,9 @@ const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulb
 */
 
 //Code Here
-let myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
+let myStrongest = monstersInYourPocket.filter(function(e) {
+  return e.CP > 200;
+});
 
 
 
