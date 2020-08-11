0e1174bba9ef179f300e15cd8c6b4cb286770773
Thang Khai <tnkhai94@gmail.com>
2020-08-10 20:59:55 -0600

    higher-order complete

--- higher-order.js
+++ higher-order.js
@@ -21,11 +21,16 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
 */
 
 //Code Here
-let evenNumbers = mixedNumbers.filter(function(element, index, wholeArray){
+let evenNumbers = mixedNumbers.filter(function(element){
   return element % 2 === 0;
 });
 
 
+
+// Arrow
+//let evenNumbers = mixedNumbers.filter(element =>{
+// element % 2 === 0; 
+//})
 
 ////////// PROBLEM 2 //////////
 
@@ -46,10 +51,11 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 */
 
 //Code Here
-let postTaxPrices = prices.map(function(element, index, wholeArray){
-  return element * 1 + (1 + (taxRate / 100));
+let postTaxPrices = prices.map(function(element){
+  return element * 1.07;
 })
-return postTaxPrices
+
+//Arrow
 
 
 
@@ -68,11 +74,13 @@ const populations = [8175133, 3792621, 2695598, 2100263];
 */
 
 //Code Here
-let totalPopulation = populations.reduce(function(runningTotal, curElement, curIndex, wholeArray){
+let totalPopulation = populations.reduce(function(runningTotal, curElement){
   return runningTotal + curElement
 })
 
-
+// let totalPopulation = populations.reduce((runningTotal, curElement) {
+// return runningTotal + curElement
+//}, 0)
 
 ////////// PROBLEM 4 //////////
 
@@ -100,7 +108,10 @@ let myStrongest = monstersInYourPocket.filter(function(e) {
   return e.CP > 200;
 });
 
-
+//Arrow
+//let myStrongest = monsterInYourPocket.filter(e => {
+//  return e.CP > 200; 
+//}) 
 
 ////////// PROBLEM 5 //////////
 
@@ -115,8 +126,9 @@ const orders = [{"price":15,"tax":0.09},{"price":42,"tax":0.07},{"price":56,"tax
   Use a higher order method to get all the order totals after adding in the sales tax (given to you as a tax rate, hint: you'll need to do some multiplication). Your answer should be an array of numbers, one total for each order.
 */
 
-let orderTotals // Code here
-
+let orderTotals = orders.map(obj => {
+  return obj.price * (1 + obj.tax)
+})
 
 
 ////////// PROBLEM 6 //////////
@@ -135,6 +147,12 @@ const purchases = [{"owner":"Barry","price":103},{"owner":"Bob","price":75},
   Use a high order method to create to get the sum of bobsTotal.
 */
 
-let bobsTotal //Code Here
+let bobsTotal = purchases.reduce((acc, obj) => {
+  if(obj.owner === "Bob") {
+    return obj.price + acc
+  } else {
+    return acc
+  }
+},0)
 
 
