5cd83be1b7926cc5d4f75b65fae56b46b398201a
Thang Khai <tnkhai94@gmail.com>
2020-08-10 20:03:22 -0600

    nesting complete

--- nesting.js
+++ nesting.js
@@ -214,4 +214,15 @@ var numsArr = [ [1, 2, 3, 4], [5, 6], [7, 8, 9, 10, 11]];
 
 //Code Here
 
-
+function looper () {
+  for( let i = 0; i < numsArr.length; i++) {
+    for(let j = 0; j < numsArr[i].length; j++){
+      if (numsArr[i][j] % 2 !== 0) {
+        numsArr[i][j] = "odd"
+      } else {
+        numsArr[i][j] = "even"
+      }
+    }
+  }
+  return numsArr
+}
